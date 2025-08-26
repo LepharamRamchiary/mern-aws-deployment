96282d6aa7224bde311726e6303f43e9b764ed6d
LepharamRamchiary <lepharamchiary@gmail.com>
2025-08-26 12:45:35 +0530

    complete the forntend

--- server/src/controllers/task.controller.js
+++ server/src/controllers/task.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { Task } from "../models/task.model.js";
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const createTask = asyncHandler(async (req, res) => {
   const { title, description } = req.body;
@@ -13,33 +13,36 @@ const createTask = asyncHandler(async (req, res) => {
 });
 
 const getAllTasks = asyncHandler(async (req, res) => {
-    const tasks = await Task.find();
-    if(!tasks){
-        throw new ApiError(404, "No tasks found");
-    }
-    res.status(200).json(new ApiResponse(200, tasks, "Tasks retrieved"));
-
-})
+  const tasks = await Task.find();
+  if (!tasks) {
+    throw new ApiError(404, "No tasks found");
+  }
+  res.status(200).json(new ApiResponse(200, tasks, "Tasks retrieved"));
+});
 
 const deleteTask = asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    
-    const task = await Task.findByIdAndDelete(id);
-    if(!task){
-        throw new ApiError(404, "Task not found");
-    }
-    res.status(200).json(new ApiResponse(200, task, "Task deleted"));
+  const { id } = req.params;
+
+  const task = await Task.findByIdAndDelete(id);
+  if (!task) {
+    throw new ApiError(404, "Task not found");
+  }
+  res.status(200).json(new ApiResponse(200, task, "Task deleted"));
 });
 
 const updateTask = asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    const { title, description } = req.body;
-    
-    const task = await Task.findByIdAndUpdate(id, { title, description }, { new: true });
-    if(!task){
-        throw new ApiError(404, "Task not found");
-    }
-    res.status(200).json(new ApiResponse(200, task, "Task updated"));
+  const { id } = req.params;
+  const { title, description, completed } = req.body;
+
+  const task = await Task.findByIdAndUpdate(
+    id,
+    { title, description, completed },
+    { new: true }
+  );
+  if (!task) {
+    throw new ApiError(404, "Task not found");
+  }
+  res.status(200).json(new ApiResponse(200, task, "Task updated"));
 });
 
-export { createTask , getAllTasks, deleteTask, updateTask};
+export { createTask, getAllTasks, deleteTask, updateTask };

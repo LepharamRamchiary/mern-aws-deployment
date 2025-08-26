53eafb11eaa1c8054c49478a975f67de1646c7c0
LepharamRamchiary <lepharamchiary@gmail.com>
2025-08-26 10:38:32 +0530

    added setup of backend

---
+++ server/src/index.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv"
+import connectDB from "./db/inedx.js"
+import { app } from "./app.js"
+
+dotenv.config({
+    path: "./.env"
+})
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed !!", err);
+    
+})

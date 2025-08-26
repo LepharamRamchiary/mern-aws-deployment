7b4ce12e29798414c5713806e06039702f21cec7
LepharamRamchiary <lepharamchiary@gmail.com>
2025-08-26 11:23:16 +0530

    added api for deleting the task

--- server/src/routers/task.route.js
+++ server/src/routers/task.route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { createTask, getAllTasks } from "../controllers/task.controller.js";
+import { createTask, getAllTasks, deleteTask } from "../controllers/task.controller.js";
 
 const router = Router();
 
 router.route("/").post(createTask).get(getAllTasks);
+router.route("/:id").delete(deleteTask);
 
 export default router;
